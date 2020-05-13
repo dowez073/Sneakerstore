d592e34fcafaea737aa730aefe586747e3522053
boaz <boazgoldsteen>
2020-05-13 11:27:07 +0200

    Add project files.

---
+++ Sneakerstore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sneakerstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
